c2e1ce23ef6da624556d6eec21501a0862184183
spark <pnw123456@icloud.com>
2023-01-31 20:13:55 +0900

    feat: Batch Configuration add

---
+++ cloud/src/main/java/com/example/boot/config/BatchConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.boot.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Configurable;
+
+@Configurable
+@EnableBatchProcessing
+public class BatchConfiguration {
+}
