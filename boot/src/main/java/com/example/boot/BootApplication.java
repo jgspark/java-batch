5e7d7da25a3a508f212845f9a46f456b5247675e
spark <pnw123456@icloud.com>
2023-01-31 20:09:23 +0900

    build: 멀티 모듈로 전환

--- boot/src/main/java/com/example/boot/BootApplication.java
+++ boot/src/main/java/com/example/boot/BootApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(BootApplication.class, args);
     }
 
 }
